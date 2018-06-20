c9d7a8381ffc2572d54332254e44f0e1b490edca
kent013 <kentaro.ishitoya@gmail.com>
2018-06-20 19:50:57 +0900

    changed limit

--- web/public/index.php
+++ web/public/index.php
@@ -13,7 +13,7 @@ function action_root() {
     foreach($articles as $k => $article){
         $articles[$k]["thumbnail_url"] = getImageURL($article);
 
-        $s_results = $pdo->query("SELECT a.*, f.title AS site_title, f.site_url, f.language, f.site_category_id, f.site_type_id, ac.primary_image_url FROM similar_articles AS sa, articles AS a, feeds AS f, article_contents AS ac WHERE sa.article_hash = '{$article['hash']}' AND sa.similar_article_hash = a.hash AND f.id = a.feed_id AND ac.article_hash = a.hash ORDER BY a.published_at DESC LIMIT 10 ");
+        $s_results = $pdo->query("SELECT a.*, f.title AS site_title, f.site_url, f.language, f.site_category_id, f.site_type_id, ac.primary_image_url FROM similar_articles AS sa, articles AS a, feeds AS f, article_contents AS ac WHERE sa.article_hash = '{$article['hash']}' AND sa.similar_article_hash = a.hash AND f.id = a.feed_id AND ac.article_hash = a.hash ORDER BY a.published_at DESC LIMIT 100");
         $similar_articles = $s_results->fetchAll();
         foreach($similar_articles as $i => $similar_article){
             $similar_articles[$i]["thumbnail_url"] = getImageURL($similar_article);

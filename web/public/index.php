b0647053bd7df4bfc9d4c4fcae4dfcbcb7925505
kent013 <kentaro.ishitoya@gmail.com>
2018-06-20 19:47:40 +0900

    brush up styles, add related content

--- web/public/index.php
+++ web/public/index.php
@@ -8,19 +8,17 @@ $klein->dispatch();
 
 function action_root() {
     $pdo = _get_connection();
-    $results = $pdo->query("SELECT a.*, f.title AS site_title, f.site_url, f.language, f.site_category_id, f.site_type_id, ac.primary_image_url FROM articles AS a, feeds AS f, article_contents AS ac WHERE f.id= a.feed_id AND ac.article_hash = a.hash ORDER BY a.published_at DESC LIMIT 500 ");
+    $results = $pdo->query("SELECT a.*, f.title AS site_title, f.site_url, f.language, f.site_category_id, f.site_type_id, ac.primary_image_url FROM articles AS a, feeds AS f, article_contents AS ac WHERE f.id= a.feed_id AND ac.article_hash = a.hash ORDER BY a.published_at DESC LIMIT 10 ");
     $articles = $results->fetchAll();
     foreach($articles as $k => $article){
-        if(empty($article["primary_image_url"])){
-            $articles[$k]["thumbnail_url"] = "https://placeimg.com/50/50/animals";
-            continue;
-        }else if(preg_match('/.gif$/', $article["primary_image_url"])){
-            $articles[$k]["thumbnail_url"] = $article["primary_image_url"];
-            continue;
+        $articles[$k]["thumbnail_url"] = getImageURL($article);
+
+        $s_results = $pdo->query("SELECT a.*, f.title AS site_title, f.site_url, f.language, f.site_category_id, f.site_type_id, ac.primary_image_url FROM similar_articles AS sa, articles AS a, feeds AS f, article_contents AS ac WHERE sa.article_hash = '{$article['hash']}' AND sa.similar_article_hash = a.hash AND f.id = a.feed_id AND ac.article_hash = a.hash ORDER BY a.published_at DESC LIMIT 10 ");
+        $similar_articles = $s_results->fetchAll();
+        foreach($similar_articles as $i => $similar_article){
+            $similar_articles[$i]["thumbnail_url"] = getImageURL($similar_article);
         }
-        $thumbnail_url = preg_replace('/https?:\/\//', '', $article["primary_image_url"]);
-        preg_match('/(https?)/', $article["primary_image_url"], $regs);
-        $articles[$k]["thumbnail_url"] = "{$regs[1]}://rsz.io/{$thumbnail_url}?width=50&aspect=1";
+        $articles[$k]["similar_articles"] = $similar_articles;
     }
     return render_template("index", ["articles" => $articles]);
 }
@@ -35,4 +33,15 @@ function render_template($template, $vars){
     $twig->addExtension(new Twig_Extensions_Extension_Text());
     $template = $twig->load("$template.twig");
     return $template->render($vars);
+}
+
+function getImageURL($article){
+    if(empty($article["primary_image_url"])){
+        return "https://placeimg.com/50/50/animals";
+    }else if(preg_match('/.gif$/', $article["primary_image_url"])){
+        return $article["primary_image_url"];
+    }
+    $thumbnail_url = preg_replace('/https?:\/\//', '', $article["primary_image_url"]);
+    preg_match('/(https?)/', $article["primary_image_url"], $regs);
+    return "{$regs[1]}://rsz.io/{$thumbnail_url}?width=50&aspect=1";
 }

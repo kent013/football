7dc8a758330d5068f7a989ca283efbd078fb6f26
kent013 <kentaro.ishitoya@gmail.com>
2018-06-21 19:32:11 +0900

    change parameter

--- web/public/index.php
+++ web/public/index.php
@@ -63,7 +63,7 @@ function getImageURL($article){
     }else if(preg_match('/.gif$/', $article["primary_image_url"])){
         return $article["primary_image_url"];
     }
-    return "https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?url={$article["primary_image_url"]}&container=focus&resize_w=50&resize_h=50&rewriteMime=image/*";
+    return "https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?url={$article["primary_image_url"]}&container=focus&resize_w=50&rewriteMime=image/*";
 }
 
 function getSettings($key = null){

4aadf4e51ac60b36439c0193db41721b219e4e4f
kent013 <kentaro.ishitoya@gmail.com>
2018-06-21 12:21:48 +0900

    fix up limit to 100

--- web/public/index.php
+++ web/public/index.php
@@ -8,12 +8,12 @@ $klein->dispatch();
 
 function action_root() {
     $pdo = _get_connection();
-    $results = $pdo->query("SELECT a.*, f.title AS site_title, f.site_url, f.language, f.site_category_id, f.site_type_id, ac.primary_image_url FROM articles AS a, feeds AS f, article_contents AS ac WHERE f.id= a.feed_id AND ac.article_hash = a.hash ORDER BY a.published_at DESC LIMIT 10 ");
+    $results = $pdo->query("SELECT a.*, f.title AS site_title, f.site_url, f.language, f.site_category_id, f.site_type_id, ac.primary_image_url FROM articles AS a, feeds AS f, article_contents AS ac WHERE f.id= a.feed_id AND ac.article_hash = a.hash ORDER BY a.published_at DESC LIMIT 100");
     $articles = $results->fetchAll();
     foreach($articles as $k => $article){
         $articles[$k]["thumbnail_url"] = getImageURL($article);
 
-        $s_results = $pdo->query("SELECT a.*, f.title AS site_title, f.site_url, f.language, f.site_category_id, f.site_type_id, ac.primary_image_url FROM similar_articles AS sa, articles AS a, feeds AS f, article_contents AS ac WHERE sa.article_hash = '{$article['hash']}' AND sa.similar_article_hash = a.hash AND f.id = a.feed_id AND ac.article_hash = a.hash ORDER BY a.published_at DESC LIMIT 100");
+        $s_results = $pdo->query("SELECT a.*, f.title AS site_title, f.site_url, f.language, f.site_category_id, f.site_type_id, ac.primary_image_url FROM similar_articles AS sa, articles AS a, feeds AS f, article_contents AS ac WHERE sa.article_hash = '{$article['hash']}' AND sa.similar_article_hash = a.hash AND f.id = a.feed_id AND ac.article_hash = a.hash ORDER BY a.published_at DESC LIMIT 10");
         $similar_articles = $s_results->fetchAll();
         foreach($similar_articles as $i => $similar_article){
             $similar_articles[$i]["thumbnail_url"] = getImageURL($similar_article);

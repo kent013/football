9e1f46d9a9ee50a36a2d94279151e857e0dec140
kent013 <kentaro.ishitoya@gmail.com>
2018-07-04 21:04:53 +0900

    fix path

--- misc/server/crawler.sh
+++ misc/server/crawler.sh
@@ -1,22 +1,26 @@
 #!/bin/bash -l
-PID_PATH=/var/log/crawler/pid$1
+MASTER_PID_PATH=/var/log/crawler/mpid$1
+CRAWLER_PID_PATH=/var/log/crawler/pid$1
 PYTHON_PATH=/home/ec2-user/.pyenv/versions/3.5.4/bin
 SCRIPT_PATH=/home/ec2-user/football/misc/server
 
-if [ -e $PID_PATH ]; then
+if [ -e $MASTER_PID_PATH ]; then
   echo "process is running."
-  cat $PID_PATH
+  cat $MASTER_PID_PATH
   exit
 fi
 
+touch $MASTER_PID_PATH
+echo $$ > $MASTER_PID_PATH
+
 while true
 do
-  if [ ! -e $PID_PATH ]; then
-    touch $PID_PATH
-    echo $$ > $PID_PATH
+  if [ ! -e $CRAWLER_PID_PATH ]; then
+    touch $CRAWLER_PID_PATH
+    echo $$ > $CRAWLER_PID_PATH
     $PYTHON_PATH/python $PYTHON_PATH/scrapy
       crawl all \
-        -a pid_path=$PID_PATH \
+        -a pid_path=$CRAWLER_PID_PATH \
         -a notify=$CRAWLER_NOTIFY \
       2>&1 | \
       /usr/sbin/rotatelogs /var/log/crawler/crawllog_%Y%m%d 86400 &

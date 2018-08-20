70adc95291eeb5124a187b253266c473ab404e71
kent013 <kentaro.ishitoya@gmail.com>
2018-08-20 17:27:29 +0900

    Fix history back button

--- web/public/assets/js/index.js
+++ web/public/assets/js/index.js
@@ -21,4 +21,34 @@ $(document).ready(function(){
         }
         return false;
     });
+
+
+  (function () {
+    var ans; //1つ前のページが同一ドメインかどうか
+    var bs = false; //unloadイベントが発生したかどうか
+    var ref = document.referrer;
+    $(window).bind("unload beforeunload", function () {
+      bs = true;
+    });
+    re = new RegExp(location.hostname, "i");
+    if (ref.match(re)) {
+      ans = true;
+    } else {
+      ans = false;
+    }
+    $('.historyback').bind("click", function () {
+      var that = this;
+      if (ans) {
+        history.back();
+        setTimeout(function () {
+          if (!bs) {
+            location.href = $(that).attr("href");
+          }
+        }, 100);
+      } else {
+        location.href = $(this).attr("href");
+      }
+      return false;
+    });
+  })();
 });
